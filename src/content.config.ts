e8fab2dba415ad3d3771ade4d9052622ed8f1bc5
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-07-10 06:56:48 -0500

    chore: cleanup of unused blog collection type

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,17 +5,6 @@ import { glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 
 // Define a 'loader' and 'schema' for each defineCollection
-const blog = defineCollection({
-  loader: glob({ pattern: "**/[^_]*.md", base: "./src/collections/blog" }),
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    description: z.string(),
-    author: z.string(),
-    tags: z.array(z.string()),
-  }),
-});
-
 const sermons = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: "./src/collections/sermons" }),
   schema: z.object({
@@ -49,4 +38,4 @@ const preachers = defineCollection({
 });
 
 // Export a single 'collections' object to register your collection(s)
-export const collections = { blog, sermons, sermonSeries, preachers };
+export const collections = { sermons, sermonSeries, preachers };
