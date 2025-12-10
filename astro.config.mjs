5cc22576aa57b3516f613c3ab8c797a91256edf6
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-12-09 22:41:40 -0600

    fix: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 import superforms from "@tmykkanen/astro-superforms-custom-pkg";
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [svelte(), icon(), superforms()]
+  integrations: [svelte(), icon(), superforms()],
+  adapter: netlify()
 });
