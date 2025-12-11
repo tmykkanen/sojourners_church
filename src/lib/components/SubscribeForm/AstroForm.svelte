c9d589834d6f2f24e5584e25f139004f9f02f745
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-12-10 18:02:50 -0600

    feat: improving form

--- src/lib/components/SubscribeForm/AstroForm.svelte
+++ src/lib/components/SubscribeForm/AstroForm.svelte
@@ -1,10 +1,10 @@
----
-export const prerender = false
-
+<script lang="ts">
 import * as Card from "$lib/components/ui/card";
 import { Input } from "$lib/components/ui/input";
 import { Button } from "$lib/components/ui/button";
 import { actions, isInputError } from "astro:actions";
+import { Toaster } from "../ui/sonner";
+import {toast} from "svelte-sonner"
 
 // const result = Astro.getActionResult(actions.subscribe);
 // const inputErrors = isInputError(result?.error) ? result.error.fields : {};
@@ -12,7 +12,28 @@ import { actions, isInputError } from "astro:actions";
 // if (result && !result.error) {
 //   console.log("success!");
 // }
----
+
+
+const handleSubmit = (event: SubmitEvent) => {
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
+
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  })
+    .then(() => {
+      console.log("Form successfully submitted")
+    toast.success(("Good job!"))
+    })
+    .catch(error => alert(error));
+};
+
+
+</script>
 
 <Card.Root class="bg-accent w-full border-none py-0 shadow-none">
   <form
@@ -20,6 +41,7 @@ import { actions, isInputError } from "astro:actions";
     class="flex flex-col gap-2"
     name="subscribe"
     netlify
+    onsubmit={handleSubmit}
   >
     <Card.Content class="flex flex-col gap-2 px-0">
       <Input
@@ -49,3 +71,5 @@ import { actions, isInputError } from "astro:actions";
     </Card.Footer>
   </form>
 </Card.Root>
+
+<Toaster />
