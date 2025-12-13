e1b8700d39d230503d879ebe5821355cd65d4b9d
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-12-13 08:42:26 -0600

    feat: began working on sermon filtering

---
+++ src/components/SermonFilter/SermonFilter.tsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { SermonFilterSelect } from "./SermonFilterSelect";
+import type { PreacherData, SeriesData, SermonData } from "@/lib/types";
+import { useStore } from "@nanostores/react";
+import { $series, $preacher, $from, $to } from "@/lib/nanostores";
+
+interface SermonFilterProps {
+  allSermonData: SermonData[];
+  allSeriesData: SeriesData[];
+  allPreachersData: PreacherData[];
+}
+
+const SermonFilter: React.FC<SermonFilterProps> = ({
+  allSermonData,
+  allSeriesData,
+  allPreachersData,
+}) => {
+  const series = useStore($series);
+  const preacher = useStore($preacher);
+
+  return (
+    <div className="flex flex-col gap-2 md:grid md:grid-cols-2">
+      <h1>Sermon Filter Component</h1>
+      <SermonFilterSelect data={allSeriesData} type="series" />
+      <SermonFilterSelect data={allPreachersData} type="preacher" />
+      <p>SERIES: {series}</p>
+      <p>PREACHER: {preacher}</p>
+    </div>
+  );
+};
+
+export { SermonFilter };
