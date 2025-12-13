548b553dc29caee49ba2f6381633233130b62d81
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-12-13 08:40:00 -0600

    feat: added support for various packages
    - astro icon
    - nanostores
    - date-fns
    - hamburger-react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), icon()]
 });
