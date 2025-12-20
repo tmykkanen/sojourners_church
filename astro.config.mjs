b7c09d414f645a3b7b4c0af6332bb3ff6ff8480e
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-12-19 22:43:40 -0600

    feat: added support for md directives using remark-directive

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
-
 import icon from "astro-icon";
 
+// deploy adapter
 import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
+// md parsing
+import remarkDirective from "remark-directive";
+import { remarkDirectiveToHTML } from "./src/lib/remark-directive-to-html";
+
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [remarkDirective, remarkDirectiveToHTML],
+  },
   site: "https://sojourners.netlify.app",
   vite: {
     plugins: [tailwindcss()],
---
+++ src/lib/remark-directive-to-html.js
@@ -0,0 +1,31 @@
+/**
+ * @import {Root} from 'mdast'
+ */
+
+import { h } from "hastscript";
+import { visit } from "unist-util-visit";
+
+// This plugin is an example to let users write HTML with directives.
+export function remarkDirectiveToHTML() {
+  /**
+   * @param {Root} tree
+   *   Tree.
+   * @returns {undefined}
+   *   Nothing.
+   */
+  return function (tree) {
+    visit(tree, function (node) {
+      if (
+        node.type === "containerDirective" ||
+        node.type === "leafDirective" ||
+        node.type === "textDirective"
+      ) {
+        const data = node.data || (node.data = {});
+        const hast = h(node.name, node.attributes || {});
+
+        data.hName = hast.tagName;
+        data.hProperties = hast.properties;
+      }
+    });
+  };
+}
