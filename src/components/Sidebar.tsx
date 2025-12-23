895b2bcbbeedcebfb04cd36415c3dd772186a9ff
tmykkanen <1270127+tmykkanen@users.noreply.github.com>
2025-12-23 17:03:05 -0600

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -24,51 +24,53 @@ const Sidebar: React.FC<SidebarProps> = ({}) => {
           <Hamburger toggled={isOpen} color="var(--muted-foreground)" />
         </DrawerTrigger>
         <DrawerOverlay className="bg-background" />
-        <DrawerContent className="mt-24 overflow-y-auto data-[vaul-drawer-direction=right]:w-full data-[vaul-drawer-direction=right]:border-none data-[vaul-drawer-direction=right]:sm:max-w-full">
-          <DrawerTitle className="sr-only">Menu</DrawerTitle>
-          <DrawerDescription className="sr-only">
-            Navigation Menu
-          </DrawerDescription>
-          <nav className="flex flex-col items-center px-4">
-            <ul className="flex flex-col gap-4">
-              {navEntries.map(({ label, path, subMenu, order }, index) =>
-                subMenu ? (
-                  <li key={index}>
-                    <ButtonLink
-                      href="#"
-                      variant="link"
-                      className="h-auto text-4xl font-bold whitespace-normal uppercase sm:text-5xl"
-                    >
-                      {label}
-                    </ButtonLink>
-                    <ul className="border-muted ml-4 flex flex-col gap-2 border-l-4">
-                      {subMenu.map((subMenuItem, index) => (
-                        <li key={index}>
-                          <ButtonLink
-                            href={`/${path}/${subMenuItem.path}`}
-                            variant="link"
-                            className="text-muted-foreground text-lg font-bold uppercase sm:text-xl"
-                          >
-                            {subMenuItem.label}
-                          </ButtonLink>
-                        </li>
-                      ))}
-                    </ul>
-                  </li>
-                ) : (
-                  <li key={index}>
-                    <ButtonLink
-                      href={`/${path}`}
-                      variant="link"
-                      className="h-auto text-4xl font-bold whitespace-normal uppercase sm:text-5xl"
-                    >
-                      {label}
-                    </ButtonLink>
-                  </li>
-                ),
-              )}
-            </ul>
-          </nav>
+        <DrawerContent className="mt-24 data-[vaul-drawer-direction=right]:w-full data-[vaul-drawer-direction=right]:border-none data-[vaul-drawer-direction=right]:sm:max-w-full">
+          <div className="overflow-y-auto">
+            <DrawerTitle className="sr-only">Menu</DrawerTitle>
+            <DrawerDescription className="sr-only">
+              Navigation Menu
+            </DrawerDescription>
+            <nav className="flex flex-col items-center px-4">
+              <ul className="flex flex-col gap-4">
+                {navEntries.map(({ label, path, subMenu, order }, index) =>
+                  subMenu ? (
+                    <li key={index}>
+                      <ButtonLink
+                        href="#"
+                        variant="link"
+                        className="h-auto text-4xl font-bold whitespace-normal uppercase sm:text-5xl"
+                      >
+                        {label}
+                      </ButtonLink>
+                      <ul className="border-muted ml-4 flex flex-col gap-2 border-l-4">
+                        {subMenu.map((subMenuItem, index) => (
+                          <li key={index}>
+                            <ButtonLink
+                              href={`/${path}/${subMenuItem.path}`}
+                              variant="link"
+                              className="text-muted-foreground text-lg font-bold uppercase sm:text-xl"
+                            >
+                              {subMenuItem.label}
+                            </ButtonLink>
+                          </li>
+                        ))}
+                      </ul>
+                    </li>
+                  ) : (
+                    <li key={index}>
+                      <ButtonLink
+                        href={`/${path}`}
+                        variant="link"
+                        className="h-auto text-4xl font-bold whitespace-normal uppercase sm:text-5xl"
+                      >
+                        {label}
+                      </ButtonLink>
+                    </li>
+                  ),
+                )}
+              </ul>
+            </nav>
+          </div>
         </DrawerContent>
       </Drawer>
     </>
